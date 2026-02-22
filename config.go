ef8b93e45df4cd0e8ab2bc4613175b2f5be88b2d
ThaseG <andrej@hyben.net>
2026-02-22 20:23:13 +0100

    Typo fix

--- config.go
+++ config.go
@@ -5,6 +5,6 @@ type Config struct {
 	OvpnTCPStatus   string `yaml:"ovpn_tcp_status"`
 	OvpnUDPStatus   string `yaml:"ovpn_udp_status"`
 	ServerName      string `yaml:"server_name"`
-	Debug           bool   `yaml:"debug"`.           // true/false default false
+	Debug           bool   `yaml:"debug"`            // true/false default false
 	RefreshInterval int    `yaml:"refresh_interval"` // in seconds, default 15
 }
